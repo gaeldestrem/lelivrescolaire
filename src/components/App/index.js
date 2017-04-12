c6981d46eb8528928d9ca97a251e2f72a87c620c
gdestrem <gael.destrem@gmail.com>
2017-04-12 15:19:42 +0200

    feat(AppComponent): include StudentAdd

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Students from '../../containers/Students'
+import StudentAdd from '../../containers/StudentAdd'
 import './styles.css'
 
 const App = () => {
     return (
       <div>
           <h1 className="title">Ma classe ! </h1>
+          <StudentAdd/>
           <Students/>
       </div>
     )
