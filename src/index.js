f573bda5f4630f6834c31e7f19c56b69fe4c89f8
gdestrem <gael.destrem@gmail.com>
2017-04-12 16:57:27 +0200

    feat(globalStyles): add global styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './components/App';
 import {Provider} from 'react-redux'
 import reducers from './reducers'
 import {createStore} from 'redux'
+import './styles/styles.css'
 
 let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
