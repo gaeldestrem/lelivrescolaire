7df25506be879f3cb3304107e7de5d1559915cf4
gdestrem <gael.destrem@gmail.com>
2017-04-12 14:59:27 +0200

    feat(AppComponent): add students

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Students from '../../containers/Students'
 import './styles.css'
 
 const App = () => {
     return (
       <div>
-          <h1 className="test">hello world</h1>
+          <h1 className="title">Ma classe ! </h1>
+          <Students/>
       </div>
     )
 }
