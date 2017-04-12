ae68a6ca62323b6584b67cdaa0927e27111c6b3f
gdestrem <gael.destrem@gmail.com>
2017-04-12 16:11:12 +0200

    chore(index.js): add redux devtools

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux'
 import reducers from './reducers'
 import {createStore} from 'redux'
 
-let store = createStore(reducers)
+let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
